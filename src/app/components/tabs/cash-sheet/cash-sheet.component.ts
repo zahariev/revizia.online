9179e46b8bfae9f09a797f3f797b32115324e873
boyan zahariev <zaharievboyan@gmail.com>
2019-05-20 01:17:06 +0300

    cashOut button position absolut

--- src/app/components/tabs/cash-sheet/cash-sheet.component.ts
+++ src/app/components/tabs/cash-sheet/cash-sheet.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "cash-sheet",
@@ -6,6 +6,8 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./cash-sheet.component.css"]
 })
 export class CashSheetComponent implements OnInit {
+  @Input()
+  data;
   constructor() {}
 
   ngOnInit() {}
