5bd3300313afa6f227b5dfe54294d1f5f5234f5d
boyan zahariev <zaharievboyan@gmail.com>
2019-05-21 07:48:39 +0300

    cash tab left margin

--- src/app/components/tabs/cash-tab/cash.component.ts
+++ src/app/components/tabs/cash-tab/cash.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: "cash-tab",
@@ -6,6 +6,11 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./cash.component.css"]
 })
 export class CashComponent implements OnInit {
+  data = {
+    deliveries: {},
+    wages: {},
+    others: {}
+  };
   constructor() {}
 
   ngOnInit() {}
