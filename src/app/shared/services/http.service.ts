c6181c77ae8cc2425787bb81dcb9f329aaecfe1f
boyan zahariev <zaharievboyan@gmail.com>
2019-06-07 18:39:53 +0300

    added setMenu - http.put request

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, throwError } from "rxjs";
+import { catchError, retry } from "rxjs/operators";
 
 import { environment } from "environments/environment";
 
@@ -8,22 +9,37 @@ import { environment } from "environments/environment";
 export class HttpService {
   baseUrl = environment.baseUrl;
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+      ApiKey: "c7a919e51f2268e5c8eb471ae093d15d"
+    })
+  };
+
   constructor(private http: HttpClient) {}
 
-  public get(url: string): Observable<any> {
-    return this.http.get(url);
+  private get(url: string, data: string): Observable<any> {
+    return this.http.post(this.baseUrl + url, data, this.httpOptions);
+  }
+
+  private set(url: string, data: string): Observable<any> {
+    return this.http.put(this.baseUrl + url, data, this.httpOptions);
   }
 
   public getMenuData(): Observable<any> {
-    return this.http.get(
-      this.baseUrl +
-        "/getMenu/?ApiKey=c7a919e51f2268e5c8eb471ae093d15d&areaID=1"
+    return this.get(
+      "/getMenu/",
+      "ApiKey=c7a919e51f2268e5c8eb471ae093d15d&areaID=1"
     );
+  }
+
+  public setMenuData(data): Observable<any> {
+    return this.set("/setMenu/", data);
   }
 
   public getRevData(): Observable<any> {
     return this.http.get(
-      "https://api.ipos.bg/ang5/itemsGet2/?ApiKey=c7a919e51f2268e5c8eb471ae093d15d&areaID=111"
+      "/itemsGet2/?ApiKey=c7a919e51f2268e5c8eb471ae093d15d&areaID=111"
     );
     //.map(response => response.text());
   }
