ed064805e37cb6521dc7d558d8d1d46a06f90075
boyan zahariev <zaharievboyan@gmail.com>
2019-06-19 02:58:06 +0300

    feat: editable tab names

--- src/app/components/tabs/tabs-menu-sheet/tabs-menu-sheet.component.ts
+++ src/app/components/tabs/tabs-menu-sheet/tabs-menu-sheet.component.ts
@@ -12,6 +12,8 @@ export class TabsMenuSheetComponent implements OnInit {
   viewList;
   editable: boolean;
   mapTabToScroll = {};
+  activeEl: any;
+  contentChange: Boolean = false;
 
   constructor(public data: RevService) {
     this.viewList = data.menuList;
@@ -30,6 +32,50 @@ export class TabsMenuSheetComponent implements OnInit {
 
     if (mlist) {
       mlist.parentElement.scrollTo(0, this.data.tabScrollPos[tabChange.index]);
+    }
+  }
+
+  onInput(ev) {
+    this.contentChange = true;
+  }
+
+  onBlur(idx, event) {
+    var el = event.target;
+    el.contentEditable = "false";
+    this.activeEl = 0;
+    // this.updateList(idx, el);
+  }
+
+  onClick(idx: any, event: any) {
+    if (this.activeEl == "select") {
+      this.makeEditable(event.target);
+      // event.preventDefault();
+    } else if (this.activeEl == event.target) {
+      this.selectText(event.target);
+      this.activeEl = "select";
+      // event.preventDefault();
+    } else {
+      this.activeEl = event.target;
+    }
+  }
+
+  makeEditable(el) {
+    if (!this.editable) return;
+    el.contentEditable = "true";
+    this.activeEl = "editable";
+  }
+
+  selectText(cell = document.activeElement) {
+    if (!this.editable) return;
+    var range, selection;
+    if (this.activeEl == "select") this.activeEl = "editable";
+    if (window.getSelection) {
+      selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents(cell);
+
+      selection.removeAllRanges();
+      selection.addRange(range);
     }
   }
 }
