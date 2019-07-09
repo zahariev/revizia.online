674b707a7d545e03e0320433a390f4e722f4a4ec
boyan zahariev <zaharievboyan@gmail.com>
2019-07-09 21:48:19 +0300

    refactor: renamed and moved tab sheet components

---
+++ src/app/components/sheets/tara/summary-sheet.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, ElementRef } from "@angular/core";
+import { Statement } from "@angular/compiler";
+import { Item } from "app/shared/models/item.model";
+
+import { RevService } from "app/shared/services/rev.service";
+import { SheetComponent } from "../sheet.component";
+
+@Component({
+  selector: "summary-sheet",
+  templateUrl: "./summary-sheet.component.html",
+  styleUrls: ["./summary-sheet.component.css"]
+})
+export class SummarySheetComponent extends SheetComponent {
+  @Input() editable: Boolean;
+  @Input() tabIdx: string;
+  @Input() tabName: string;
+
+  viewList;
+
+  constructor(public dat: RevService, public el: ElementRef) {
+    super(dat, el);
+
+    this.columnList.length = 3;
+  }
+
+  ngOnInit() {
+    // console.log(this.dat.viewSheet);
+    // this.dataList = this.dat.revizia[this.date];
+  }
+}
